70e01c279f8f283cb0ff51ca92425aed70dc52ca
badri119 <badrinathv98@gmail.com>
2023-12-14 01:48:50 -0800

    added a small para

--- src/components/Starting/Starting.jsx
+++ src/components/Starting/Starting.jsx
@@ -9,7 +9,7 @@ function Starting() {
         </div>
         <div className="text">
           <p>
-            <span className="span1">
+            <span>
               Hey there, 1401 gang! So, I've got this crazy idea. How about we
               shake off the dust and get our legs moving with a group run? Yep,
               you heard it right! Picture this: us, pounding the pavement,
@@ -20,12 +20,22 @@ function Starting() {
             <br></br>
             <br></br>
 
-            <span className="span2">
+            <span>
               I think this run will be really fun! It's not just about
               exercising - it's also about having a good time and getting to
               hang out. Sound interesting to you? If you're interested, fill in
               the form below, and if you've got any thoughts, spill the beans!
               I'm all ears for your feedback, good or bad.
+            </span>
+
+            <br></br>
+            <br></br>
+
+            <span>
+              I'm a newbie at running and I absolutely despise doing it alone
+              (hopefully, you're in the same boat). Let's be real, running with
+              a bunch of people will totally light a fire under our lazy butts!
+              Team work makes the dream work right?
             </span>
           </p>
         </div>
